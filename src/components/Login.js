a48d8506a27d0f78f31489c8a26c5edbb3657abf
Alan Mir <alanpaulphotos@gmail.com>
2021-02-21 00:51:29 -0500

    Created state, and handleChange event handler

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+
+initialState = {
+  username: "",
+  password: ""
+};
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [user, setUser] = useState(initialState);
+
+  const handleChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  
 
   useEffect(()=>{
     axios
