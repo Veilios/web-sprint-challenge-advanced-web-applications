5030fa1b87daca3371689f174df125c6a87fb1f3
Alan Mir <alanpaulphotos@gmail.com>
2021-02-21 04:44:37 -0500

    Commented out unnecesarrey code from Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,25 +44,25 @@ const Login = () => {
       });
   }
 
-  useEffect(()=>{
-    axios
-      .delete(`http://localhost:5000/api/colors/1`, {
-        headers:{
-          'authorization': "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98"
-        }
-      })
-      .then(res=>{
-        axios.get(`http://localhost:5000/api/colors`, {
-          headers:{
-            'authorization': ""
-          }
-        })
-        .then(res=> {
-          console.log(res);
-        });
-        console.log(res);
-      })
-  });
+  // useEffect(()=>{
+  //   axios
+  //     .delete(`http://localhost:5000/api/colors/1`, {
+  //       headers:{
+  //         'authorization': "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98"
+  //       }
+  //     })
+  //     .then(res=>{
+  //       axios.get(`http://localhost:5000/api/colors`, {
+  //         headers:{
+  //           'authorization': ""
+  //         }
+  //       })
+  //       .then(res=> {
+  //         console.log(res);
+  //       });
+  //       console.log(res);
+  //     })
+  // });
 
   return (
     <>
